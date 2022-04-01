694cf4ff60e20b937fa12ae37ce67eca0988e470
pnandez <paquiherca@gmail.com>
2022-04-01 11:24:13 +0100

    Test give the same number if there is only one given in red

---
+++ __test__/StringCalculator.spec.ts
@@ -0,0 +1,8 @@
+import { Main } from '../src/Stringcalculator';
+
+describe('String calculator should', () => {
+  it('give the same number if there is only one given', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.sum('1')).toBe(1);
+  });
+});
